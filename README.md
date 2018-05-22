53e10a6704cd799879e1890fb8db19f82f32890c
Chloee Robertson <me.c.leigh@gmail.com>
2018-05-22 12:11:12 -0700

    Added a readme. Rolling one dice now works.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# ASPNET-dnd
+# ASP.NET-dnd
 A personal project for a D&amp;D web application using ASP.NET Core 2.0 and SQL Server
+
+
+## Helpful tutorials used along the way:
+
+* [Dropdowns in .Net](https://www.c-sharpcorner.com/article/binding-dropdown-list-with-database-in-asp-net-core-mvc/) - Used for the roll dropdowns
--- WebApp_DnD/Controllers/RollController.cs
+++ WebApp_DnD/Controllers/RollController.cs
@@ -3,13 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApp_DnD.Data;
+using WebApp_DnD.Models.DnD;
 
 namespace WebApp_DnD.Controllers
 {
     public class RollController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public RollController(ApplicationDbContext context) {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
+            List<Die> dice = new List<Die>();
+
+            dice = _context.Dice.ToList();
+
+            ViewBag.ListOfDice = dice;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Die die) {
+
+            ViewBag.SelectedValue = die.NumSides;
+
+            Random rnd = new Random();
+            ViewBag.rolledValue = rnd.Next(1, die.NumSides + 1);
+
+            List<Die> dice = new List<Die>();
+            dice = _context.Dice.ToList();
+            ViewBag.ListOfDice = dice;
+
+
             return View();
         }
     }
