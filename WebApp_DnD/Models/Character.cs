1b9840ba839fdcb97f8a9d856f5b8c2622c1cf87
Chloee Robertson <me.c.leigh@gmail.com>
2018-10-08 17:57:17 -0700

    Added some characteristics to the Character class. Updated some the Details page and added some initial data to the DB.

--- WebApp_DnD/Models/Character.cs
+++ WebApp_DnD/Models/Character.cs
@@ -19,18 +19,7 @@ namespace WebApp_DnD.Models
         public int ExperiencePoints { get; set; }
         public int Level { get; set; }
 
-        [Range(1,20)]
-        public int Strength { get; set; }
-        [Range(1, 20)]
-        public int Dexterity { get; set; }
-        [Range(1, 20)]
-        public int Consitution { get; set; }
-        [Range(1, 20)]
-        public int Intelligence { get; set; }
-        [Range(1, 20)]
-        public int Wisdom { get; set; }
-        [Range(1, 20)]
-        public int Charisma { get; set; }
+        
 
         [ForeignKey("User")]
         public ApplicationUser AppUser { get; set; }
@@ -50,6 +39,49 @@ namespace WebApp_DnD.Models
         public int Speed { get; set; }
         public int MaxHitPoints { get; set; }
 
+        public int Inspiration { get; set; }
+        public int ProficiencyBonus { get; set; }
+
+        [Range(1,20)]
+        public int Strength { get; set; }
+        [Range(1, 20)]
+        public int Dexterity { get; set; }
+        [Range(1, 20)]
+        public int Consitution { get; set; }
+        [Range(1, 20)]
+        public int Intelligence { get; set; }
+        [Range(1, 20)]
+        public int Wisdom { get; set; }
+        [Range(1, 20)]
+        public int Charisma { get; set; }
+
+        //Saving Throws
+        public int ST_Strength { get; set; }
+        public int ST_Dexterity { get; set; }
+        public int ST_Consitution { get; set; }
+        public int ST_Intelligence { get; set; }
+        public int ST_Wisdom { get; set; }
+        public int ST_Charisma { get; set; }
+
+        //Skills
+        public int Acrobatics { get; set; }
+        public int AnimalHandling { get; set; }
+        public int Arcana { get; set; }
+        public int Athletics { get; set; }
+        public int Deception { get; set; }
+        public int History { get; set; }
+        public int Insight { get; set; }
+        public int Intimidation { get; set; }
+        public int Investigation { get; set; }
+        public int Medicine { get; set; }
+        public int Nature { get; set; }
+        public int Perception { get; set; }
+        public int Performance { get; set; }
+        public int Persuasion { get; set; }
+        public int Religion { get; set; }
+        public int SleightOfHand { get; set; }
+        public int Stealth { get; set; }
+        public int Survival { get; set; }
 
         public IEnumerable<CharacterEquipment> Equipment { get; set; }
         public IEnumerable<Proficiency> Proficiency { get; set; }
